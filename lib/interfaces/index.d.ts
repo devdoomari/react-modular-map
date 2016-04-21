73bcdd2540b118aa3ea6d731ac7c6324a3aea5f7
meshkorea-dev <no-preply@meshkorea.net>
2016-04-21 18:03:56 +0000

    generated from:0a06aa3529e6b12e9730c4efa88f198aea40d5d0 triggered by:devdoomari [ci skip]

---
+++ lib/interfaces/index.d.ts
@@ -0,0 +1,5 @@
+export { IPoint } from './point';
+export { ILatLng } from './latlng';
+export { IDimension } from './dimension';
+export { IBehavior } from './behavior';
+export { IBoundsChangedHandler, IZoomLevelChangedHandler, ICenterChangedHandler } from './map_provider';
