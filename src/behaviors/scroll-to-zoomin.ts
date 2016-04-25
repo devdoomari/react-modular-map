5282485801814c9c3cba8960c15b30aa7c178f2d
Dev@Mesh::iMac <devdoomari@gmail.com>
2016-04-25 18:36:22 +0900

    [ux fix] set center to [currentCenter * 0.7 + cursurLatLng * 0.3] on scroll

--- src/behaviors/scroll-to-zoomin.ts
+++ src/behaviors/scroll-to-zoomin.ts
@@ -12,12 +12,17 @@ export default class ScrollToZoomIn implements IBehavior {
   initialize(eventsStream: Rx.Subject<any>, controller: MapController) {
     const mouseWheelStream = eventsStream.filter(
       event => event.type === 'wheel'
-    ).throttleTime(300);
+    ).throttleTime(600);
 
     mouseWheelStream.subscribe((event) => {
       const left = event.offsetX;
       const top = event.offsetY;
-      const newCenter = controller.pointToLatLng({left, top});
+      const cursorAt = controller.pointToLatLng({left, top});
+      const center = controller.getCenter();
+      const newCenter = {
+        lat: (cursorAt.lat * 0.3) + (center.lat * 0.7),
+        lng: (cursorAt.lng * 0.3) + (center.lng * 0.7),
+      };
       controller.setCenter(newCenter);
       let zoom = controller.getZoomLevel();
       if (event.deltaY > 0) {
