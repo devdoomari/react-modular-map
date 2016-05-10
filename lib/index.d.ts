ad7eb854618b7f5298bcba84ec5cc0b61f0c598e
meshkorea-dev <no-preply@meshkorea.net>
2016-05-10 14:46:55 +0000

    generated from:55c45c374764b6a76d70a309f08107966b002504 triggered by:devdoomari [ci skip]

---
+++ lib/index.d.ts
@@ -0,0 +1,8 @@
+export { default as MapBounds } from './types/bounds';
+export { default as BaseMapProvider } from './types/base-map-provider';
+export { default as ControlledMap } from './controlled-map';
+export { default as Map } from './map';
+import * as Interfaces from './interfaces';
+export { Interfaces };
+import * as Behaviors from './behaviors';
+export { Behaviors };
---
+++ lib/interfaces/latlng.d.ts
@@ -0,0 +1,5 @@
+export interface ILatLng {
+    lat: Number;
+    lng: Number;
+    crs?: String;
+}
