c7a2aa3833b153fe00826840eb025b35e14fada1
meshkorea-dev <no-preply@meshkorea.net>
2016-05-11 02:38:00 +0000

    generated from:ac034ac3641472ac3b3c41dda52e7895882045ef triggered by:devdoomari [ci skip]

---
+++ typings/main.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="main/ambient/core-decorators/index.d.ts" />
+/// <reference path="main/ambient/eventemitter2/index.d.ts" />
+/// <reference path="main/ambient/lodash/index.d.ts" />
+/// <reference path="main/ambient/q/index.d.ts" />
+/// <reference path="main/ambient/react-bootstrap/index.d.ts" />
+/// <reference path="main/ambient/react-dom/index.d.ts" />
+/// <reference path="main/ambient/react/index.d.ts" />
